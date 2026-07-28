ee15e8d00204073a5f02649823f9453c8816de23
shelena91 <pretim2@outlook.com>
2026-07-28 20:38:08 -0300

    Initialize App component with routing and navigation

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import BottomNav from './components/BottomNav';
+import Painel from './pages/Painel';
+import Produtos from './pages/Produtos';
+import Links from './pages/Links';
+import Comissoes from './pages/Comissoes';
+import Calendario from './pages/Calendario';
+
+export default function App() {
+  return (
+    <HashRouter>
+      <div className="mx-auto min-h-full max-w-md px-4 pb-24 pt-4">
+        <Routes>
+          <Route path="/" element={<Painel />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/links" element={<Links />} />
+          <Route path="/comissoes" element={<Comissoes />} />
+          <Route path="/calendario" element={<Calendario />} />
+        </Routes>
+      </div>
+      <BottomNav />
+    </HashRouter>
+  );
+}
