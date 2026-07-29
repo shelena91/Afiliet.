db240ad8f4ef5b0e0b319265ead62f21c6208a1a
shelena91 <pretim2@outlook.com>
2026-07-29 20:22:47 -0300

    Implement EmailGate for access control
    Added EmailGate component to control access based on unlock state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import BottomNav from './components/BottomNav';
+import EmailGate, { isUnlocked } from './components/EmailGate';
 import Painel from './pages/Painel';
 import Produtos from './pages/Produtos';
 import Links from './pages/Links';
@@ -7,6 +9,12 @@ import Comissoes from './pages/Comissoes';
 import Calendario from './pages/Calendario';
 
 export default function App() {
+  const [liberado, setLiberado] = useState(isUnlocked());
+
+  if (!liberado) {
+    return <EmailGate onUnlock={() => setLiberado(true)} />;
+  }
+
   return (
     <HashRouter>
       <div className="mx-auto min-h-full max-w-md px-4 pb-24 pt-4">
